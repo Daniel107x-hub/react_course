2c1195c8a508db488200911bb0c908b215b63e38
Daniel Mejia <daniel107x@outlook.es>
2022-07-28 07:49:54 -0500

    Started meals project

---
+++ projects/p1-react-meals/src/components/Meals/MealsSummary.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import classes from './MealsSummary.module.css';
+
+function MealsSummary() {
+  return (
+    <section className={classes.summary}>
+        <h2>Delicious food delivered to you!</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae accusantium aspernatur quo facere officia nisi perferendis molestiae unde esse quasi.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci maiores consectetur maxime dolore quidem natus ad perspiciatis autem, est voluptatum.</p>
+    </section>
+  )
+}
+
+export default MealsSummary
