a2f491e33f77f07619fd7181cb768a57aab5118b
daniel107x <DMejiaV310796@outlook.com>
2023-08-02 21:51:31 -0600

    Added styling

--- virtual-assistants/src/App.js
+++ virtual-assistants/src/App.js
@@ -1,31 +1,54 @@
-import 'bulma/css/bulma.css';
+import "bulma/css/bulma.css";
 import ProfileCard from "./ProfileCard";
-import AlexaImage from './images/alexa.png';
-import CortanaImage from './images/cortana.png';
-import SiriImage from './images/siri.png';
+import AlexaImage from "./images/alexa.png";
+import CortanaImage from "./images/cortana.png";
+import SiriImage from "./images/siri.png";
 
 function App() {
   return (
     <div>
-      <div>Personal digital assistants</div>
+      <Hero />
       <div className="container">
         <section className="section">
           <div className="columns">
-            <div className="column is-3">
-              <ProfileCard title="Alexa" handle="@alexa99" image={AlexaImage}></ProfileCard>
+            <div className="column is-4">
+              <ProfileCard
+                title="Alexa"
+                handle="@alexa99"
+                image={AlexaImage}
+                description="Alexa was created by Amazon and helps you buy things"
+              ></ProfileCard>
             </div>
-            <div className="column is-3">
-              
-            <ProfileCard title="Cortana" handle="@cortana32" image={CortanaImage}></ProfileCard>
+            <div className="column is-4">
+              <ProfileCard
+                title="Cortana"
+                handle="@cortana32"
+                image={CortanaImage}
+                description="Cortana was created by Microsoft What does it do?"
+              ></ProfileCard>
             </div>
-            <div className="column is-3">
-              
-            <ProfileCard title="Siri" handle="@siri01" image={SiriImage}></ProfileCard>
+            <div className="column is-4">
+              <ProfileCard
+                title="Siri"
+                handle="@siri01"
+                image={SiriImage}
+                description="Siri was created by Apple"
+              ></ProfileCard>
             </div>
           </div>
         </section>
       </div>
     </div>
+  );
+}
+
+function Hero() {
+  return (
+    <section class="hero is-primary">
+      <div class="hero-body">
+        <p class="title">Personal digital assistants</p>
+      </div>
+    </section>
   );
 }
 
--- virtual-assistants/src/ProfileCard.js
+++ virtual-assistants/src/ProfileCard.js
@@ -1,29 +1,30 @@
-import React from 'react'
+import React from "react";
 
-function ProfileCard({title, handle, image}) {
-    // const {title, handle} = props; //Destructuring
-    return (
-        <div className="card">
-            <div className="card-image">
-                <figure className="image is-1by1">
-                    <img src={image} alt="PDA Logo" />
-                </figure>
-            </div>
-            <div className="card-content">
-                <div className="media">
-                    <div className="media-left">
-                        <figure className="image is-48x48">
-                            <img src={image} alt="PDA Logo" />
-                        </figure>
-                    </div>
-                    <div className="media-content">
-                        <p className="title is-4">{title}</p>
-                        <p className="subtitle is-6">{handle}</p>
-                    </div>
-                </div>
-            </div>
+function ProfileCard({ title, handle, image, description }) {
+  // const {title, handle} = props; //Destructuring
+  return (
+    <div className="card">
+      <div className="card-image">
+        <figure className="image is-1by1">
+          <img src={image} alt="PDA Logo" />
+        </figure>
+      </div>
+      <div className="card-content">
+        <div className="media">
+          <div className="media-left">
+            <figure className="image is-48x48">
+              <img src={image} alt="PDA Logo" />
+            </figure>
+          </div>
+          <div className="media-content">
+            <p className="title is-4">{title}</p>
+            <p className="subtitle is-6">{handle}</p>
+          </div>
         </div>
-    )
+        <div className="content">{description}</div>
+      </div>
+    </div>
+  );
 }
 
-export default ProfileCard
+export default ProfileCard;
