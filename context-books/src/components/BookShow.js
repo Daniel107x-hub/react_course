6c5f14825a79eae147fb0da3a8d545a420af76a0
daniel107x <DMejiaV310796@outlook.com>
2023-08-12 11:03:10 -0600

    Final books application

--- context-books/src/components/BookShow.js
+++ context-books/src/components/BookShow.js
@@ -21,6 +21,7 @@ function BookShow({ book, onDelete, onEdit }) {
 
   return (
     <div className="book-show">
+      <img src={`https://picsum.photos/seed/${book.id}/300/200`} alt="books" />
       <div className="actions">
         <button className="delete" onClick={handleDelete}>
           Delete
