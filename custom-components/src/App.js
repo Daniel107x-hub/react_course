bd73bf43757f93d13d8ef60f92b03c315ecd057c
Daniel Mejia <Daniel.Mejia2@Honeywell.com>
2023-08-22 08:16:28 -0600

    Added react icons

--- custom-components/src/App.js
+++ custom-components/src/App.js
@@ -1,19 +1,28 @@
 import React from "react";
 import Button from "./Button";
+import { GoBellFill, GoCloud, GoDatabase } from "react-icons/go";
 
 function App() {
+  const handleClick = () =>{
+    console.log("Button clicked")
+  }
   return (
     <div>
       <div>
-        <Button primary rounded outline>
+        <Button primary onClick = {handleClick} className="mb-5">
+          <GoBellFill/>
           Click me
         </Button>
       </div>
       <div>
-        <Button secondary>Buy now!</Button>
+        <Button secondary>
+          <GoCloud/>
+          Buy now!</Button>
       </div>
       <div>
-        <Button success>See deal!</Button>
+        <Button success>
+          <GoDatabase/>
+          See deal!</Button>
       </div>
       <div>
         <Button warning>Hide ads!</Button>
@@ -22,7 +31,7 @@ function App() {
         <Button danger>Something</Button>
       </div>
       <div>
-        <Button>Danger zone</Button>
+        <Button  plain>Danger zone</Button>
       </div>
     </div>
   );
--- custom-components/src/Button.js
+++ custom-components/src/Button.js
@@ -11,18 +11,25 @@ function Button({
   danger,
   outline,
   rounded,
+  ...rest
 }) {
-  const finalClassName = className("px-3 py-1.5", {
-    "border-1": outline,
-    "bg-blue-500 text-white border-red-500": primary,
-    "bg-gray-900 text-white": secondary,
-    "bg-green-500 text-black": success,
-    "bg-yellow-400 text-black": warning,
-    "bg-red-500 text-black": danger,
+  const finalClassName = className(rest.className, 'flex items-center px-3 py-1.5 border', {
+    'text-black border-black': plain,
+    "border-blue-500 bg-blue-500 text-white": primary,
+    "border-gray-900 bg-gray-900 text-white": secondary,
+    "border-green-500 bg-green-500 text-black": success,
+    "border-yellow-400 bg-yellow-400 text-black": warning,
+    "border-red-500 bg-red-500 text-black": danger,
     "rounded-full": rounded,
+    "bg-white": outline,
+    'text-blue-500': outline && primary,
+    'text-gray-900': outline && secondary,
+    'text-green-500': outline && success,
+    'text-yellow-500': outline && warning,
+    'text-red-500': outline && danger,
   });
 
-  return <button className={finalClassName}>{children}</button>;
+  return <button className={finalClassName} {...rest}>{children}</button>;
 }
 Button.propTypes = {
   checkVariationValue: ({
