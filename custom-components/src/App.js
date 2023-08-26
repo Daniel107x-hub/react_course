ed1022508c40e809d751c1786ea480fa3d0da106
daniel107x <DMejiaV310796@outlook.com>
2023-08-26 11:20:21 -0600

    Added prop removal from button component

--- custom-components/src/App.js
+++ custom-components/src/App.js
@@ -3,26 +3,28 @@ import Button from "./Button";
 import { GoBellFill, GoCloud, GoDatabase } from "react-icons/go";
 
 function App() {
-  const handleClick = () =>{
-    console.log("Button clicked")
-  }
+  const handleClick = () => {
+    console.log("Button clicked");
+  };
   return (
     <div>
       <div>
-        <Button primary onClick = {handleClick} className="mb-5">
-          <GoBellFill/>
+        <Button primary onClick={handleClick} className="mb-1">
+          <GoBellFill />
           Click me
         </Button>
       </div>
       <div>
         <Button secondary>
-          <GoCloud/>
-          Buy now!</Button>
+          <GoCloud />
+          Buy now!
+        </Button>
       </div>
       <div>
         <Button success>
-          <GoDatabase/>
-          See deal!</Button>
+          <GoDatabase />
+          See deal!
+        </Button>
       </div>
       <div>
         <Button warning>Hide ads!</Button>
@@ -31,7 +33,7 @@ function App() {
         <Button danger>Something</Button>
       </div>
       <div>
-        <Button  plain>Danger zone</Button>
+        <Button plain>Danger zone</Button>
       </div>
     </div>
   );
