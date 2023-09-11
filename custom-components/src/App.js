6f1be06544b1984d5989bc81376147f22c1825f9
daniel107x <DMejiaV310796@outlook.com>
2023-09-11 08:04:49 -0600

    Custom navigation: Added Link and Route components

--- custom-components/src/App.js
+++ custom-components/src/App.js
@@ -1,6 +1,22 @@
 import React from "react";
+import Link from "./components/Link";
+import AccordionPage from "./pages/AccordionPage";
+import DropdownPage from "./pages/DropdownPage";
+import Route from "./components/Route";
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Link to={"/accordion"}>Go to accordion</Link>
+      <Link to={"/dropdown"}>Go to dropdown</Link>
+      <Route path={"/accordion"}>
+        <AccordionPage />
+      </Route>
+      <Route path={"/dropdown"}>
+        <DropdownPage />
+      </Route>
+    </div>
+  );
 }
 
 export default App;
