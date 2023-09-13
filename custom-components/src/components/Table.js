3bb7b54c63785fe75900416206ed39e6537251c9
daniel107x <DMejiaV310796@outlook.com>
2023-09-12 21:48:51 -0600

    Table: Initial styling

--- custom-components/src/components/Table.js
+++ custom-components/src/components/Table.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from "react";
 
-function Table({data}) {
+function Table({ data }) {
+  const renderedRows = data.map((item) => {
+    return (
+      <tr className="border-b" key={item.name}>
+        <td className="p-3">{item.name}</td>
+        <td className="p-3">
+          <div className={`p-3 m-2 ${item.color}`}></div>
+        </td>
+        <td className="p-3">{item.score}</td>
+      </tr>
+    );
+  });
   return (
-    <div>{data.length}</div>
-  )
+    <table className="table-auto border-spacing-2">
+      <thead>
+        <tr className="border-b-2">
+          <th>Fruit</th>
+          <th>Color</th>
+          <th>Score</th>
+        </tr>
+      </thead>
+      <tbody>{renderedRows}</tbody>
+    </table>
+  );
 }
 
-export default Table
+export default Table;
