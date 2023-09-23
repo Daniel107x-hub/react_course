fa72130f30a109621718852aec7dff9f80818404
Daniel Mejia <Daniel.Mejia2@Honeywell.com>
2023-09-23 14:40:52 -0600

    Finished sortable table

--- custom-components/src/components/SortableTable.js
+++ custom-components/src/components/SortableTable.js
@@ -1,21 +1,59 @@
 import React, { useState } from "react";
 import Table from "./Table";
+import {FaSort, FaSortUp, FaSortDown} from 'react-icons/fa'
 
-function SortableTable(props) {
-  const handleSort = (column) => {};
-  const { config } = props;
+function SortableTable({data, config,  ...rest}) {
+  const [sortOrder, setSortOrder] = useState(null);
+  const [sortBy, setSortBy] = useState(null);
+
+  const handleSort = (column) => {
+    if(sortOrder === null || sortBy !== column.label) {
+      setSortOrder('asc')
+      setSortBy(column.label)
+    }else if(sortOrder === 'asc'){
+      setSortOrder('desc');
+      setSortBy(column.label);
+    }else{
+      setSortOrder(null);
+      setSortBy(null);
+    }
+  };
+
   const updatedConfig = config.map((column) => {
     if (!column.sortValue) return column;
+    const icon = sortOrder === null || column.label !== sortBy ? <FaSort/> : sortOrder === 'asc' ? <FaSortUp/> : <FaSortDown/>
     return {
       ...column,
       header: () => (
-        <th className="bg-red-500" onClick={() => handleSort(column)}>
-          {column.label} is sortable
+        <th onClick={() => handleSort(column)} className="flex items-center cursor-pointer hover:text-red-600" >
+          {icon}{column.label}
         </th>
       ),
     };
   });
-  return <Table {...props} config={updatedConfig} />;
+
+  // Only sort data if soprt order and sort by are not null
+  // Make a copy of data prop
+  // Bind correct sortValue function and use it for sorting
+
+  let sortedData = data;
+  if(sortOrder && sortBy){
+    const {sortValue} = config.find(col => col.label === sortBy);
+    sortedData = [...data].sort((a, b) => {
+      const valueA = sortValue(a);
+      const valueB = sortValue(b);
+      const reversedOrder = sortOrder === 'asc' ? 1 : -1;
+      if(typeof valueA === 'string'){
+        return valueA.localeCompare(valueB) * reversedOrder;
+      }else{
+        return (valueA - valueB) * reversedOrder;
+      }
+    })
+  }
+
+  return <div>
+    <Table data={sortedData} {...rest} config={updatedConfig} />
+  </div>
 }
 
 export default SortableTable;
--- custom-components/src/components/Table.js
+++ custom-components/src/components/Table.js
@@ -1,5 +1,4 @@
-import React, { useState } from "react";
-import Button from "./Button";
+import React from "react";
 import { Fragment } from "react";
 
 function Table({ data, config, keyFnc }) {
